adb3d9a943759fa0a14687655dc10483cd8640ab
doan <you@example.com>
2023-08-11 16:31:57 +0700

    uload oc

--- src/Page/Client/Home/Product.tsx
+++ src/Page/Client/Home/Product.tsx
@@ -1,40 +1,154 @@
-import styles from '../../../css/Home/product.module.css'
-
-
+import styles from "../../../css/Home/product.module.css";
+import Carousel, { slidesToShowPlugin, autoplayPlugin } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+import { Col, Row } from "antd";
+import React from 'react'
 const Product = () => {
-    return (
-        <div className={styles.list_products}>
-            <h3>Sản phẩm nổi bật</h3>
-            <div className={styles['show-product']}>
 
-            <div className={styles['products']}>
-                <div className={styles['product-photo']}>
-                    <img src="https://thuthuatnhanh.com/wp-content/uploads/2022/08/ao-thun-in-hinh-theo-yeu-cau.jpg" alt="" />
-                </div>
-                <span className={styles['product-sale']}>-30%</span>
-                <div className={styles['product-title']}>
-                    <span className={styles['product-name']}>Áo thu mùa đông đẹp tuyệt vời sang xịn mịn</span>
-                    <div className={styles['product-price']}>
-                        <span>30.000đ</span>
-                        <span>Đã bán : 30k</span>
+    const data = [
+        {
+            id: 1,
+            name: "Áo thu mùa đông đẹp tuyệt vời sang xịn mịn 1",
+            image:
+                "https://thuthuatnhanh.com/wp-content/uploads/2022/08/ao-thun-in-hinh-theo-yeu-cau.jpg",
+            price: "31000",
+            sale: 13,
+        },
+        {
+            id: 2,
+            name: "Áo thu mùa đông đẹp tuyệt vời sang xịn mịn 2",
+            image:
+                "https://thuthuatnhanh.com/wp-content/uploads/2022/08/ao-thun-in-hinh-theo-yeu-cau.jpg",
+            price: "32000",
+            sale: 5,
+        },
+        {
+            id: 3,
+            name: "Áo thu mùa đông đẹp tuyệt vời sang xịn mịn 3",
+            image:
+                "https://down-vn.img.susercontent.com/file/febb990a8125cc09630ba1fe6a744f83",
+            price: "33000",
+            sale: 9,
+        },
+        {
+            id: 4,
+            name: "Áo thu mùa đông đẹp tuyệt vời sang xịn mịn 4",
+            image:
+                "https://thuthuatnhanh.com/wp-content/uploads/2022/08/ao-thun-in-hinh-theo-yeu-cau.jpg",
+            price: "34000",
+            sale: 14,
+        },
+        {
+            id: 5,
+            name: "Áo thu mùa đông đẹp tuyệt vời sang xịn mịn 5",
+            image:
+                "https://thuthuatnhanh.com/wp-content/uploads/2022/08/ao-thun-in-hinh-theo-yeu-cau.jpg",
+            price: "35000",
+            sale: 12,
+        },
+        {
+            id: 6,
+            name: "Áo thu mùa đông đẹp tuyệt vời sang xịn mịn 6",
+            image:
+                "https://innhanh24h.net/wp-content/uploads/2019/05/%C3%81o-thun-n%E1%BB%AF-hot-%C4%91%E1%BA%B9p.jpg",
+            price: "36000",
+            sale: 8,
+        },
+        {
+            id: 7,
+            name: "Áo thu mùa đông đẹp tuyệt vời sang xịn mịn 7",
+            image:
+                "https://www.sangia.vn/uploads/Image/2020/11/14/%C3%81o%20thun%20in%20h%C3%ACnh%20%C4%91%E1%BA%B9p.jpg",
+            price: "37000",
+            sale: 10,
+        },
+    ];
+    return (
+        <div>
+            <div className={styles.list_products}>
+                <h4>Sản phẩm nổi bật</h4>
+                <div className="css-library">
+                    <div className={styles["show-product"]}>
+                        <Carousel
+                            id='2'
+                            animationSpeed={2000}
+                            plugins={[
+                                'infinite',
+                                {
+                                    resolve: autoplayPlugin,
+                                    options: {
+                                        interval: 2000,
+                                    }
+                                },
+                                "arrows",
+                                {
+                                    resolve: slidesToShowPlugin,
+                                    options: {
+                                        numberOfSlides: 5,
+                                    },
+                                },
+                            ]}
+
+
+                        >
+                            {data.map((item: any, index: any) => {
+                                return (
+                                    <div
+                                        className={styles["products"]}
+                                        style={{ marginLeft: index == 0 ? 0 : 10 }}
+                                    >
+                                        <div className={styles["product-photo"]}>
+                                            <img src={item.image} alt="" />
+                                        </div>
+                                        <span className={styles["product-sale"]}>-{item.sale}%</span>
+                                        <div className={styles["product-title"]}>
+                                            <span className={styles["product-name"]}>{item.name}</span>
+                                            <div className={styles["product-price"]}>
+                                                <span>{item.price}đ</span>
+                                                <span>Đã bán : 30k</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </Carousel>
+
                     </div>
                 </div>
             </div>
-            <div className={styles['products']} style={{marginLeft:20}}>
-                <div className={styles['product-photo']}>
-                    <img src="https://www.sangia.vn/uploads/Image/2020/11/14/%C3%81o%20thun%20in%20h%C3%ACnh%20%C4%91%E1%BA%B9p.jpg" alt="" />
-                </div>
-                <div className={styles['product-title']} >
-                    <span className={styles['product-name']}>Áo thu mùa đông đẹp tuyệt vời sang xịn mịn</span>
-                    <div className={styles['product-price']}>
-                        <span>30.000đ</span>
-                        <span>Đã bán : 30k</span>
+            <div className={styles.list_products}>
+                <h4>Sản phẩm của Shop</h4>
+                <div className="css-library">
+                    <div className={styles["show-product"]}>
+                        <Row gutter={16}>
+
+                            {data.map((item: any, index: any) => {
+                                return (
+                                    <Col xs={12} sm={8} md={6} lg={4} xl={6}>
+                                        <div
+                                            className={styles["products"]}
+                                        >
+                                            <div className={styles["product-photo"]}>
+                                                <img src={item.image} alt="" />
+                                            </div>
+                                            <span className={styles["product-sale"]}>-{item.sale}%</span>
+                                            <div className={styles["product-title"]}>
+                                                <span className={styles["product-name"]}>{item.name}</span>
+                                                <div className={styles["product-price"]}>
+                                                    <span>{item.price}đ</span>
+                                                    <span>Đã bán : 30k</span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Col>
+                                );
+                            })}
+                        </Row>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Product
+export default Product;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,27 @@
 import styles from '../css/Home/home.module.css'
-import React, {  } from 'react'
+import React, { useEffect } from 'react'
 import { Input, Badge, } from 'antd';
 import { UserOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 const { Search } = Input;
 const Header: React.FC = () => {
 
     const onSearch = (value: any) => console.log(value);
+    useEffect(() => {
+        const sticky = document.querySelectorAll("#navbar").offsetTop;
+        console.log(sticky,'sticky')
+        if (sticky) {
+            window.onscroll = async function () {
+                window.pageYOffset >= sticky
+                    ? document.querySelectorAll("#navbar").classList.add("sticky")
+                    : document.querySelectorAll("#navbar").classList.remove("sticky");
+            };
+        }
+    }, []);
     return (
         <div className={styles.header}>
-            <div className='header-content'>
+            <div className='header-content' id="navbar">
                 <div className={styles.logo}>
-                    <img src="https://firebasestorage.googleapis.com/v0/b/imageshopee-e80ee.appspot.com/o/images%2FSHOP-NTS%20(1).png?alt=media&token=3d5d8bfc-fd43-4970-b363-717670dcd5b9" alt="" />
+                    <img src="https://firebasestorage.googleapis.com/v0/b/order-94f58.appspot.com/o/images%2FSHOP-NTS%20(2).png?alt=media&token=7aa8ec58-398f-4ec0-96bc-577f6f5d5ca8" alt="" />
                 </div>
                 <div className='header-input'>
                     <Search
